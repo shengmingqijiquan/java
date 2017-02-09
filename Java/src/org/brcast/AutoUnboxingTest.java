2cb016bacf5e8edbe9f5f161b1b9d49fd99a8566
shengmingqijiquan <1127641712@qq.com>
2017-02-09 21:45:13 +0800

    Initial commit

---
+++ Java/src/org/brcast/AutoUnboxingTest.java
@@ -0,0 +1,14 @@
+package org.brcast;
+
+/**
+ * 功能：Java基本数据类型之自动装箱测试
+ * 作者：生命奇迹泉
+ * 博客：http://blog.csdn.net/shengmingqijiquan
+ * Github：https://github.com/shengmingqijiquan/2017job
+ * 时间：2017.02.09
+ */
+public class AutoUnboxingTest {
+    public static void main(String[] args){
+
+    }
+}
